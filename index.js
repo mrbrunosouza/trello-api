a5a8e9f07a139b34997b83ae40a6fb5e9735ac4a
Bruno Souza <mr.brunosouza@gmail.com>
2021-09-06 18:19:11 -0300

    alterado index.js para porta

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ server.use(routes);
 
 
 server.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port, server.settings.env);
 });
