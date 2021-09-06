c943c04bb181043aa7b5a4a8a63293357b35d037
Bruno Souza <mr.brunosouza@gmail.com>
2021-09-06 18:29:45 -0300

    adicionado cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv/config');
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const server = express();
+
+server.use(cors());
 
 server.use(express.json());
 
